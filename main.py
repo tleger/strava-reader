bf2ffc2670a1f3b82824abb5ee9ac251672d92de
tleger <thomasleger1@gmail.com>
2024-09-12 21:58:27 +0100

    refresh requirements.txt

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=10000)
